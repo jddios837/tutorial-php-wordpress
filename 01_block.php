d6ac35b83846a3e6e68fc118887cfd5c875ddb36
Juan Gomez <zatkiel837@gmail.com>
2018-10-14 15:21:45 -0400

    first commit

---
+++ 01_block.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Aprendiendo PHP</title>
+</head>
+<body>
+	<?php
+		echo "Hola mundo";
+
+		echo "<br/>";
+		
+		echo 20 + 20;
+	?>
+
+</body>
+</html>
